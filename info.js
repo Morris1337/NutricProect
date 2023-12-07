f69d7874902c8bd63fbcc514c38e9ebca708ea38
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2023-12-07 14:40:04 +0200

    update

--- info.js
+++ info.js
@@ -1,28 +1,6 @@
-function infoIMT(){
+const fuckU = document.querySelector("#imtInfoBtn");
+const imtInfo = document.getElementById("imtInfo");
 
-let one = "Острый дефици массы: <16";
-let two = "Недостатоная масса тела: 16 - 18.5";
-let thre = "Норма: 18.6 - 25";
-let four = "Избыточная масса тела: 25.1 - 30";
-let five = "Ожирение первой степени: 30.1 - 35";
-let six = "Ожирение второй степени: 35.1 - 40";
-let seven = "Ожирение третей степени: 40>";
-
-    document.getElementById("hInfoIMTOne").innerHTML = ("ИМТ (Индекс массы тела)")
-    document.getElementById("infoIMTOne").innerHTML = (one)
-    document.getElementById("infoIMTTwo").innerHTML = (two)
-    document.getElementById("infoIMTThre").innerHTML = (thre)
-    document.getElementById("infoIMTFour").innerHTML = (four)
-    document.getElementById("infoIMTFive").innerHTML = (five)
-    document.getElementById("infoIMTSix").innerHTML = (six)
-    document.getElementById("infoIMTSeven").innerHTML = (seven)
-
-    // "Острый дефици массы: " + "<16",
-// "Недостатоная масса тела: " + "16 - 18.5",
-// "Норма: " + "18.6 - 25",
-// "Избыточная масса тела: " + "25.1 - 30",
-// "Ожирение первой степени: " + "30.1 - 35",
-// "Ожирение второй степени: " + "35.1 - 40",
-// "Ожирение третей степени: " + "40>"
-
-}
+fuckU.addEventListener("click", () => {
+    imtInfo.classList.toggle("open");
+});
--- nutric.js
+++ nutric.js
@@ -1,4 +1,6 @@
 
+
+
 function startFunc(){
 
 let client = {
